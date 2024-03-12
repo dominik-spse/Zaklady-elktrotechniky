95df2bfa9ffd466f7aea1133be7c6f534ec1dbc3
dominik-spse <162470672+dominik-spse@users.noreply.github.com>
2024-03-12 10:43:28 +0100

    Základy-2
    Upravená verze

--- zakladyelektrotechniky/zakladyelektrotechniky/Program.cs
+++ zakladyelektrotechniky/zakladyelektrotechniky/Program.cs
@@ -22,10 +22,10 @@ namespace zakladyelektrotechniky
             Console.Title = "Základy elektrotechniky";
 
             Console.WriteLine("Vítejte v aplikaci pro elektrotechnické výpočty.");
-            Console.BackgroundColor = ConsoleColor.Red;
+           /* Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Všechny hodnoty pište v celých číslech");
-            Console.ResetColor();
-            Console.WriteLine("Napište číslo jedné z následujících možností: \n1.Rezistory \n2.Výkon \n3.Napětí \n4.Proud \n5.Odpor\n6.Odejít  ");
+            Console.ResetColor();*/
+            Console.WriteLine("Napište číslo jedné z následujících možností: \n1.Rezistory \n2.Výkon \n3.Napětí \n4.Proud \n5.Odpor\n6.Měrný odpor vodiče\n7.Odejít  ");
             Console.Write("Vybraná možnost: ");
             int vstup = Convert.ToInt16(Console.ReadLine());
             Console.WriteLine(vstup);
@@ -52,11 +52,42 @@ namespace zakladyelektrotechniky
                     Odpor();
                     break;
                 case 6:
+                    Console.Clear();
+                    Vodic();
+                    break;
+                case 7:
                     Environment.Exit(0);
                     break;
-
-
-
+            }
+            if (vstup != 1||vstup!=2||vstup!=3||vstup!=4||vstup!=5||vstup!=6||vstup!=7){
+                Console.WriteLine("Napiš sem poze číslo z nabídky");
+                Thread.Sleep(1500);
+                hlavni();
+            }
+        }
+        static void Vodic()
+        {
+            Console.Clear();
+            Console.WriteLine("Výpočet měrného odporu drátu R=p*(l/S)");
+            Console.Write("Zadejte měrný odpor vodiče v  Ωmm^2m^-1: ");
+            double odpor= Convert.ToDouble(Console.ReadLine());
+            Console.Write("Zadejte délku vodiče v metrech: ");
+            double delka= Convert.ToDouble(Console.ReadLine());
+            Console.Write("Zadejte průřez vodiče v mm^2: ");
+            double pruzez= Convert.ToDouble(Console.ReadLine());
+            double celyOdpor = odpor * (delka / pruzez);
+            Console.WriteLine("Celkový odpor vodiče je " + celyOdpor + " ohmů");
+            Console.WriteLine("Další možnosti\n1.Na hlavní stránku\n2.Zpět na výkon");
+            Console.Write("Vybraná možnost");
+            int moznosti = Convert.ToInt16(Console.ReadLine());
+            switch (moznosti)
+            {
+                case 1:
+                    hlavni();
+                    break;
+                case 2:
+                    Vodic();
+                    break;
             }
         }
         static void Odpor()
@@ -64,10 +95,10 @@ namespace zakladyelektrotechniky
             Console.Clear();
             Console.WriteLine("Výpočet proudu R=U/I");
             Console.Write("Zadejte hodnotu napětí ve voltech: ");
-            int napeti = Convert.ToInt32(Console.ReadLine());
+            double napeti = Convert.ToDouble(Console.ReadLine());
             Console.Write("Zadejte hodnotu proudu v ampérech: ");
-            int proud = Convert.ToInt32(Console.ReadLine());
-            int odpor = napeti / proud;
+            double proud = Convert.ToDouble(Console.ReadLine());
+            double odpor = napeti / proud;
             Console.WriteLine("Celkový odpor je " + proud + " ohmů");
             Console.WriteLine("Další možnosti\n1.Na hlavní stránku\n2.Zpět na výkon");
             Console.Write("Vybraná možnost");
@@ -87,10 +118,10 @@ namespace zakladyelektrotechniky
             Console.Clear();
             Console.WriteLine("Výpočet proudu I=U/R");
             Console.Write("Zadejte hodnotu napětí ve voltech: ");
-            int napeti = Convert.ToInt32(Console.ReadLine());
+            double napeti = Convert.ToDouble(Console.ReadLine());
             Console.Write("Zadejte hodnotu odporu v ohmech: ");
-            int odpor = Convert.ToInt32(Console.ReadLine());
-            int proud = napeti / odpor;
+            double odpor = Convert.ToDouble(Console.ReadLine());
+            double proud = napeti / odpor;
             Console.WriteLine("Celkový proud je " + proud + " ampérů");
             Console.WriteLine("Další možnosti\n1.Na hlavní stránku\n2.Zpět na výkon");
             Console.Write("Vybraná možnost");
@@ -111,10 +142,10 @@ namespace zakladyelektrotechniky
             Console.Clear();
             Console.WriteLine("Výpočet napětí U=I*R");
             Console.Write("Zadejte proud v ampérech: ");
-            int proud = Convert.ToInt16(Console.ReadLine());
+            double proud = Convert.ToDouble(Console.ReadLine());
             Console.Write("Zadejte odpor v ohmech: ");
-            int odpor = Convert.ToInt16(Console.ReadLine());
-            int napeti = proud * odpor;
+            double odpor = Convert.ToDouble(Console.ReadLine());
+            double napeti = proud * odpor;
             Console.WriteLine("Napětí je " + napeti + " voltů");
             Console.WriteLine("Další možnosti\n1.Na hlavní stránku\n2.Zpět na výkon");
             Console.Write("Vybraná možnost");
@@ -135,33 +166,33 @@ namespace zakladyelektrotechniky
             Console.WriteLine("Výpočet el. výkonu\n1.P=U*I\n2.P=(U^2)/R\n3.P=(I^2)/R");
             Console.Write("Vybraná možnost: ");
             int vstup = Convert.ToInt16(Console.ReadLine());
-            int napeti = 0;
-            int proud = 0;
-            int odpor = 0;
-            int vykon1 = 0;
+            double napeti = 0;
+            double proud = 0;
+            double odpor = 0;
+            double vykon1 = 0;
             switch (vstup)
             {
                 case 1:
                     Console.Write("Zadejte napětí ve voltech: ");
-                    napeti = Convert.ToInt16(Console.ReadLine());
+                    napeti = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Zadejte proud v ampérech: ");
-                    proud = Convert.ToInt16(Console.ReadLine());
+                    proud = Convert.ToDouble(Console.ReadLine());
                     vykon1 = napeti * proud;
                     Console.WriteLine("Výkon je " + vykon1 + " Watů");
                     break;
                 case 2:
                     Console.Write("Zadejte napětí ve voltech: ");
-                    napeti = Convert.ToInt16(Console.ReadLine());
+                    napeti = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Zadejte odpor v ohmech: ");
-                    odpor = Convert.ToInt16(Console.ReadLine());
+                    odpor = Convert.ToDouble(Console.ReadLine());
                     vykon1 = (napeti * napeti) / odpor;
                     Console.WriteLine("Výkon je " + vykon1 + " Watů");
                     break;
                 case 3:
                     Console.Write("Zadejte proud v ampérech: ");
-                    proud = Convert.ToInt16(Console.ReadLine());
+                    proud = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Zadejte odpor v ohmech: ");
-                    odpor = Convert.ToInt16(Console.ReadLine());
+                    odpor = Convert.ToDouble(Console.ReadLine());
                     vykon1 = (proud * proud) / odpor;
                     Console.WriteLine("Výkon je " + vykon1 + " Watů");
                     break;
@@ -191,15 +222,15 @@ namespace zakladyelektrotechniky
             switch (inRezistory)
             {
                 case 1:
-                    int[] poleRezistor = { 0, 0, 0, 0, 0 };
+                    double[] poleRezistor = new double[5];
                     Console.WriteLine("Zadejte hodnotu odporu v ohmech, pokud tam není, zapište 0 ");
                     for (int i = 0; i < poleRezistor.Length; i++)
                     {
 
                         Console.Write("Zadejte hodnotu " + (i + 1) + ". rezistoru ");
-                        int poleVstup = poleRezistor[i] = Convert.ToInt32(Console.ReadLine());
+                        double poleVstup = poleRezistor[i] = Convert.ToDouble(Console.ReadLine());
                     }
-                    int hodnotaS = poleRezistor.Sum();
+                    double hodnotaS = poleRezistor.Sum();
                     Console.WriteLine("Celkový odpor paralelně zapojených odporů je: " + hodnotaS + " ohmů");
                     break;
                 //nebyl jsem schopnej to vymyslet, tak mi stím AI pomohla od tuď
@@ -248,16 +279,23 @@ namespace zakladyelektrotechniky
                 hlavni();
 
             }
-            catch (FormatException)
+            catch (FormatException e)
             {
                 Console.Clear();
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("Nastala chyba. Zpracovávání, může trvat několik minut.");
-                Thread.Sleep(6000);
+
+                for(int i = 0; i < 7; i++)
+                {
+
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+                }
+                Thread.Sleep(1000);
                 Console.WriteLine("Chyba nalezena");
                 Thread.Sleep(2000);
-                Console.WriteLine("Chyba je samozřejmě v tobě, když jsi se pokusil zadat písmeno nebo znak tam, kde má být číslo, blbče.\nChceš se vrátit zpátky a zkusit to znovu?");
+                Console.WriteLine("Chyba je samozřejmě v tobě, když jsi se pokusil zadat písmeno nebo znak tam, kde má být číslo, @#!!!.\nChceš se vrátit zpátky a zkusit to znovu?");
                 string anoNe = Console.ReadLine();
                 if (anoNe == "ano" || anoNe == "Ano" || anoNe == "aNo" || anoNe == "anO" || anoNe == "ANo" || anoNe == "AnO" || anoNe == "aNO" || anoNe == "ANO")
                 {
